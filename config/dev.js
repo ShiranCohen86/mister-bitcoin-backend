096aa6d357d2de1552481a498559f479319955db
ShiranCohen86 <shiranc86@gmail.com>
2021-10-04 12:44:27 +0300

    first comit

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  dbURL:
+    "mongodb+srv://shiran:miko12345@cluster0.fna2u.mongodb.net/misterBitcoinDB?retryWrites=true&w=majority",
+
+  // 'dbURL': 'mongodb://localhost:27017',
+};
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  dbURL:
+    "mongodb+srv://shiran:miko12345@cluster0.fna2u.mongodb.net/misterBitcoinDB?retryWrites=true&w=majority",
+};
