1021c0983b09311a0828c6995db02e985d8d9905
ShiranCohen86 <shiranc86@gmail.com>
2021-10-10 23:58:04 +0300

    slight-change

--- api/contact/contact.service.js
+++ api/contact/contact.service.js
@@ -1,6 +1,5 @@
 const dbService = require("../../services/db.service");
 const ObjectId = require("mongodb").ObjectId;
-const asyncLocalStorage = require("../../services/als.service");
 const logger = require("../../services/logger.service");
 
 module.exports = {
